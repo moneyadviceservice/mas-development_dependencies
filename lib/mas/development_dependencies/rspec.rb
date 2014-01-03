c79bf0affe0ad9396446f75e30bd39b696d7812e
Jakub Novotny <novotny.jakub@gmail.com>
2014-01-03 15:44:55 +0000

    Add more shared rspec config

--- lib/mas/development_dependencies/rspec.rb
+++ lib/mas/development_dependencies/rspec.rb
@@ -2,6 +2,11 @@ require 'rspec/rails'
 require 'shoulda-matchers'
 require 'timecop'
 
+Time.zone = 'Europe/London'
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('./../factories/**.rb')].each { |f| require f }
+
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods if defined?(FactoryGirl)
   config.include Devise::TestHelpers, :type => :controller if defined?(Devise)
@@ -13,4 +18,12 @@ RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values  = true
   config.run_all_when_everything_filtered                 = true
   config.filter_run :focus
+
+  config.before(:each) do
+    I18n.locale = :en
+  end
+
+  config.after(:each) do
+    Timecop.return
+  end
 end
