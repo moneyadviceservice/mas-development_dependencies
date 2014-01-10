de391bf84590a90853f75574ba84e8ec3843238a
Andrew Garner <andrew@andrewgarner.com>
2014-01-10 12:10:11 +0000

    Add rake task

--- lib/mas/development_dependencies/konacha.rb
+++ lib/mas/development_dependencies/konacha.rb
@@ -7,6 +7,15 @@ module MAS
       initializer 'mas.konacha.environment', before: 'konacha.environment' do
         config.konacha.driver = :poltergeist
       end
+
+      rake_tasks do
+        namespace 'spec' do
+          desc 'Run the code examples in spec/javascript'
+          task :javascript => :environment do
+            exit 1 unless Konacha.run
+          end
+        end
+      end
     end if defined?(Rails::Railtie)
   end
 end
