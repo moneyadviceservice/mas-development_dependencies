b85b0c307389d8100063874e6846427af4871523
Andrew Garner <andrew@andrewgarner.com>
2014-01-15 11:02:38 +0000

    Move example Cucumber features to dummy
    Run cucumber using MAS Development Dependencies as we would expect a
    consuming application to.

--- features/support/env.rb
+++
@@ -1,2 +0,0 @@
-ENV["RAILS_ROOT"] ||= File.dirname(__FILE__) + "../../../spec/dummy"
-require 'mas/development_dependencies/cucumber/env'
--- spec/dummy/features/cucumber.feature
+++ spec/dummy/features/cucumber.feature
@@ -1,4 +1,5 @@
 Feature: Cucumber Testing Helper
+
   Scenario: Cucumber Works
     Given I am running a feature
     Then it should pass
