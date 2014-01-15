e7cab4a5540d30335b794c80291fd04f071ddb7d
Andrew Garner <andrew@andrewgarner.com>
2014-01-15 15:38:13 +0000

    Avoid Guard::DSL monkeypatch by using a generator

--- lib/mas/development_dependencies.rb
+++ lib/mas/development_dependencies.rb
@@ -1,8 +1,9 @@
 require 'mas/development_dependencies/version'
 
 require 'mas/development_dependencies/cucumber'
-require 'mas/development_dependencies/rspec'
+require 'mas/development_dependencies/guard'
 require 'mas/development_dependencies/konacha'
+require 'mas/development_dependencies/rspec'
 
 module MAS
   module DevelopmentDependencies
--- lib/mas/development_dependencies/guard.rb
+++ lib/mas/development_dependencies/guard.rb
@@ -1,42 +1,21 @@
 require 'guard'
+require 'guard/livereload'
+require 'guard/sass'
 
-module Guard
-  class Dsl
-
-    MAS_DEFINITIONS = [:sass, :livereload]
-
-    def mas_guard(*args)
-      if args.empty?
-        MAS_DEFINITIONS.each { |arg| send arg }
-      else
-        args.each do |arg|
-          unless MAS_DEFINITIONS.include?(arg.to_sym)
-            ::Guard::UI.warning("No default MAS configuration for plugin \"#{arg}\"")
-            next
-          end
-          send arg
+module MAS
+  module DevelopmentDependencies
+    module Guard
+      class Railtie < Rails::Railtie
+        generators do
+          require File.expand_path('../../../generators/guard/install', __FILE__)
         end
-      end
-    end
 
-    private
-    def sass
-      require 'guard/sass'
-      guard 'sass', input: 'app/assets/stylesheets', output: 'public/stylesheets'
-    end
+        initializer 'mas.rack.live_reload' do |app|
+          require 'rack-livereload'
 
-    def livereload
-      require 'guard/livereload'
-      require 'rack-livereload'
-
-      guard 'livereload' do
-        watch(%r{^app/.+\.(erb|haml|js|css|scss|sass|coffee|eco|png|gif|jpg)})
-        watch(%r{^app/helpers/.+\.rb})
-        watch(%r{public/.+\.(css|js|html)})
-        watch(%r{^config/locales/.+\.yml})
-        # Rails Assets Pipeline
-        watch(%r{(app|vendor)(/assets/\w+/(.+\.(erb|haml|js|css|scss|sass|coffee|eco|png|gif|jpg))).*}) { |m| "/assets/#{m[3]}" }
-      end
+          app.middleware.use Rack::LiveReload
+        end
+      end if defined?(Rails::Railtie)
     end
   end
 end
