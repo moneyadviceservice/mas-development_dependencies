3cfa1241e56d6f63603f43eb34b9e01835ee4024
Jakub Novotny <novotny.jakub@gmail.com>
2014-01-20 17:08:05 +0000

    Version 2.0.0

--- lib/mas/development_dependencies/version.rb
+++ lib/mas/development_dependencies/version.rb
@@ -1,8 +1,8 @@
 module MAS
   module DevelopmentDependencies
     module Version
-      MAJOR = 1
-      MINOR = 4
+      MAJOR = 2
+      MINOR = 0
       PATCH = 0
 
       STRING = [MAJOR, MINOR, PATCH].join('.')
