5d79fdb48cf40c90f49ae15a1cd8a80718d9982b
Andrew Garner <andrew@andrewgarner.com>
2014-02-06 17:00:34 +0000

    Fix Konacha namespace collision

--- lib/mas/development_dependencies/konacha.rb
+++ lib/mas/development_dependencies/konacha.rb
@@ -17,7 +17,7 @@ module MAS
           namespace 'spec' do
             desc 'Run the code examples in spec/javascript'
             task :javascript => :environment do
-              exit 1 unless Konacha.run
+              exit 1 unless ::Konacha.run
             end
           end
         end
