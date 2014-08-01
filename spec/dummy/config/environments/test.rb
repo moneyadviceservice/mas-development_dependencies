29fe9da643d6b9e1c733f1c0f099976289f209aa
Jared Fraser <dev@jsf.io>
2014-08-01 15:10:08 +0100

    Remove unneeded activerecord dummy config

--- spec/dummy/config/environments/test.rb
+++ spec/dummy/config/environments/test.rb
@@ -23,9 +23,6 @@ Dummy::Application.configure do
   # Disable request forgery protection in test environment
   config.action_controller.allow_forgery_protection    = false
 
-  # Raise exception on mass assignment protection for Active Record models
-  config.active_record.mass_assignment_sanitizer = :strict
-
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
 end
