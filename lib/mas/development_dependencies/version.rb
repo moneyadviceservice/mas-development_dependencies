de00ee49c85b92f24cc4bb1de273c991ae90b964
Phil Lee <asmega@ph-lee.com>
2014-08-18 15:18:18 +0100

    bump version to 2.3.0

--- lib/mas/development_dependencies/version.rb
+++ lib/mas/development_dependencies/version.rb
@@ -2,8 +2,8 @@ module MAS
   module DevelopmentDependencies
     module Version
       MAJOR = 2
-      MINOR = 2
-      PATCH = 1
+      MINOR = 3
+      PATCH = 0
 
       STRING = [MAJOR, MINOR, PATCH].join('.')
     end
